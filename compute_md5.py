271cd19ddce15e7d63bf43702328be38b60f3083
Sergey Tushev <sergy5@mail.ru>
2025-05-10 12:41:32 +0300

    do not recompute MD5 if file is already in cache

--- compute_md5.py
+++ compute_md5.py
@@ -6,6 +6,15 @@ from argparse import ArgumentParser
 import hashlib
 from multiprocessing import Pool
 from tqdm.auto import tqdm
+from typing import Tuple, List
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,  # Уровень логирования (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Формат вывода
+    datefmt='%Y-%m-%d %H:%M:%S'  # Формат даты и времени
+)
 
 
 def calculate_md5(file_path):
@@ -25,6 +34,22 @@ def init_args():
     return parser
 
 
+def get_existing_cache(filename: str) -> List[Tuple[str, str]]:
+    try:
+        with open(filename) as f_in:
+            cache_md5 = [s.strip().split('\t') for s in f_in.readlines()]
+    except FileNotFoundError:
+        cache_md5 = None
+    
+    return cache_md5
+            
+
+def get_files_not_in_cache(cache: List[Tuple[str, str]], files: List[str]) -> List[str]:
+    cached_files = set([item[0] for item in cache])
+    files = list(set(files) - cached_files)
+    return files
+
+
 def main(args):
     files_to_process = list()
     for root,_, files in os.walk(args.input_dir):
@@ -32,13 +57,23 @@ def main(args):
             full_path = os.path.join(root, f)
             files_to_process.append(full_path)
 
+    logging.info(f"files found: {len(files_to_process)}")
+    cache_md5 = get_existing_cache(args.result_file)
+
+    logging.info(f"cached_files: {len(cache_md5)}")
+    files_to_process = get_files_not_in_cache(cache_md5, files_to_process)
+    logging.info(f"new files for compute cache md5: {len(files_to_process)}")
+
     with Pool(args.num_threads) as pool:
-        results = list(tqdm(pool.imap_unordered(calculate_md5, files_to_process), 
+        results = list(tqdm(pool.imap_unordered(calculate_md5, files_to_process, chunksize=10), 
                             total=len(files_to_process)))
         
-    print('save md5 to file')
-    with open(args.result_file, 'w') as f_out:
-        f_out.writelines(list(map(lambda x: f'{x[0]}\t{x[1]}\n', results)))
+    if results:
+        cache_md5.extend(results)
+        
+        logging.info("Update cache MD5 file")
+        with open(args.result_file, 'w') as f_out:
+            f_out.writelines(list(map(lambda x: f'{x[0]}\t{x[1]}\n', cache_md5)))
 
 
 if __name__ == '__main__':
